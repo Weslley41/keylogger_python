18df0adf4fcb55628be6658c89211003c30e22ca
weslley41 <41weslley@gmail.com>
2022-08-23 16:16:05 -0300

    add Exceptions

--- connection.py
+++ connection.py
@@ -1,7 +1,9 @@
 import json
 import os
-import sys
-import mysql.connector
+try:
+	import mysql.connector
+except ImportError as err:
+  raise Exception("[!] Error: Modules not found. Please run 'pipenv sync'") from err
 
 class Connection():
 	_connection = None
@@ -11,19 +13,15 @@ class Connection():
 			mysql_configs = json.loads(os.getenv('MYSQL_CONFIG'))
 			connection = mysql.connector.connect(**mysql_configs)
 		except TypeError:
-			print("Can't load .env configs")
-			sys.exit()
+			raise Exception("[!] Can't load .env configs")
 		except mysql.connector.Error as error:
 			match error.errno:
 				case 1045:
-					print(f"Access denied to user: {mysql_configs['user']}")
-					sys.exit()
+					raise Exception(f"[!] Access denied to user: {mysql_configs['user']}") from error
 				case 1049:
-					print(f"Can't connect to database: {mysql_configs['database']}")
-					sys.exit()
+					raise Exception(f"[!] Can't connect to database: {mysql_configs['database']}") from error
 				case _:
-					print(error)
-					sys.exit()
+					raise Exception(error) from error
 
 		self._connection = connection
 		return self._connection.cursor(dictionary=True)
