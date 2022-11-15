c54260b8fb0086a1556f5a5cea58cbd0b2a6a607
weslley41 <41weslley@gmail.com>
2022-11-15 18:59:01 -0300

    rename routes and format code

--- bin/keylogger_cli.py
+++ bin/keylogger_cli.py
@@ -8,124 +8,130 @@ import requests
 
 
 def show_menu():
-	keyboard_count = requests.get('http://localhost:8000/count/', timeout=10).text
-	os.system('clear')
-	print(' Keylogger '.center(50, '-'))
-	print(f'Today: {date.today()}'.ljust(24), f'Counted keys: {keyboard_count}'.rjust(25))
-	print('-'*50)
+    keyboard_count = requests.get(HOSTNAME + 'get_use_count/', timeout=10).text
+    os.system('clear')
+    print(' Keylogger '.center(50, '-'))
+    print(f'Today: {date.today()}'.ljust(24),
+		f'Counted keys: {keyboard_count}'.rjust(25))
+    print('-'*50)
 
-	print('\n\
-  1. Show the most used keys\n\
-  2. Get log file\n\
-  3. Show weekly graphic\n\
-  4. Exit\n')
+    print(
+        "1. Show the most used keys\n"
+        "2. Get log file\n"
+        "3. Show weekly graphic\n"
+        "4. Exit\n"
+    )
 
-	option = input('Choice one: ')
-	if option.isdigit() and int(option) in range(1, 5):
-		return int(option)
+    option = input('Choice one: ')
+    if option.isdigit() and int(option) in range(1, 5):
+        return int(option)
 
-	print('Invalid option.')
-	sleep(1.5)
-	return show_menu()
+    print('Invalid option.')
+    sleep(1.5)
+    return show_menu()
 
 
 def press_to_back():
-  input('Press Enter to back.')
+    input('Press Enter to back.')
 
 
 def input_date():
-	os.system('clear')
-	print('tricks:\n\
+    os.system('clear')
+    print('tricks:\n\
   0: back\n\
   today: show today\'s data')
 
-	day = input('Choice a date(YYYY-MM-DD): ')
+    day = input('Choice a date(YYYY-MM-DD): ')
 
-	match day:
-		case 'today':
-			os.system('clear')
-			return date.today()
-		case '0':
-			return False
-		case _:
-			re_matches = re.search(DATE_REGEX, day)
-			if re_matches:
-				os.system('clear')
-				return re_matches.group(0)
+    match day:
+        case 'today':
+            os.system('clear')
+            return date.today()
+        case '0':
+            return False
+        case _:
+            re_matches = re.search(DATE_REGEX, day)
+            if re_matches:
+                os.system('clear')
+                return re_matches.group(0)
 
-			print('Invalid date format.')
-			sleep(1.5)
-			input_date()
+            print('Invalid date format.')
+            sleep(1.5)
+            input_date()
 
 
 def format_json_to_text(title, day, json_data):
-	string = ' Keylogger '.center(50, '-') + '\n'
-	string += f'{title}'.ljust(25)
-	string += f'Day: {day}'.rjust(25) + '\n'
-	string += '-' * 50 + '\n'
+    string = ' Keylogger '.center(50, '-') + '\n'
+    string += f'{title}'.ljust(25)
+    string += f'Day: {day}'.rjust(25) + '\n'
+    string += '-' * 50 + '\n'
 
-	for key in json_data:
-		string += key.ljust(40) + str(json_data[key]).rjust(10) + '\n'
+    for key in json_data:
+        string += key.ljust(40) + str(json_data[key]).rjust(10) + '\n'
 
-	if not json_data:
-		string += 'Data not found\n'.center(50)
+    if not json_data:
+        string += 'Data not found\n'.center(50)
 
-	return string
+    return string
 
 
 def show_most_used_keys():
-	day = input_date()
-	if day:
-		data = requests.get(f'http://localhost:8000/top_keys/?day={day}', timeout=10).json()
-		print(format_json_to_text('Most used keys', day, data))
-		press_to_back()
+    day = input_date()
+    if day:
+        data = requests.get(
+            HOSTNAME + f'get_most_used_keys/?day={day}', timeout=10).json()
+        print(format_json_to_text('Most used keys', day, data))
+        press_to_back()
 
 
 def get_log_file():
-	day = input_date()
-	if day:
-		data = requests.get(f'http://localhost:8000/daily_log/?day={day}', timeout=10).json()
-		if data:
-			filename = f'{Path.home()}/keyboard_logs/keyboard_log_{day}.txt'
-			os.makedirs(os.path.dirname(filename), exist_ok=True)
-			with open(filename, 'w', encoding='UTF-8') as log_file:
-				log_file.write(format_json_to_text('Log file', day, data))
+    day = input_date()
+    if day:
+        data = requests.get(
+            HOSTNAME + f'get_used_keys/?day={day}', timeout=10).json()
+        if data:
+            filename = f'{Path.home()}/keyboard_logs/keyboard_log_{day}.txt'
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+            with open(filename, 'w', encoding='UTF-8') as log_file:
+                log_file.write(format_json_to_text('Log file', day, data))
 
-			print(f'Log as saved in {filename}\n')
-		else:
-			print('Data not found\n')
+            print(f'Log as saved in {filename}\n')
+        else:
+            print('Data not found\n')
 
-		press_to_back()
+        press_to_back()
 
 
 def show_weekly_graphic():
-	start, end = date.today() - timedelta(days=6), date.today()
-	data = requests.get(f'http://localhost:8000/interval_log/{start}/to/{end}', timeout=10).json()
+    start, end = date.today() - timedelta(days=6), date.today()
+    data = requests.get(
+        HOSTNAME + f'get_interval_use_count/{start}/to/{end}/', timeout=10).json()
 
-	plt.figure("Keylogger graphic", figsize=(8, 4.5))
-	plt.plot(data.keys(), data.values())
-	plt.title(f'Weekly graphic: {start} to {end}')
-	plt.grid(True)
-	plt.show()
+    plt.figure("Keylogger graphic", figsize=(8, 4.5))
+    plt.plot(data.keys(), data.values())
+    plt.title(f'Weekly graphic: {start} to {end}')
+    plt.grid(True)
+    plt.show()
 
 
 def main():
-	run_cli = True
+    run_cli = True
 
-	while run_cli:
-		option = show_menu()
+    while run_cli:
+        option = show_menu()
 
-		match option:
-			case 1:
-				show_most_used_keys()
-			case 2:
-				get_log_file()
-			case 3:
-				show_weekly_graphic()
-			case 4:
-				run_cli = False
+        match option:
+            case 1:
+                show_most_used_keys()
+            case 2:
+                get_log_file()
+            case 3:
+                show_weekly_graphic()
+            case 4:
+                run_cli = False
 
 
 if __name__ == '__main__':
-  DATE_REGEX = r'(\d{4})-(\d{2}|\d)-(\d{2}|\d)'
-  main()
+    DATE_REGEX = r'(\d{4})-(\d{2}|\d)-(\d{2}|\d)'
+    HOSTNAME = 'http://localhost:1234/'
+    main()
--- keylogger_api.py
+++ keylogger_api.py
@@ -1,13 +1,13 @@
 from datetime import date, timedelta, datetime
 
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from keys import Keyboard
 
 app = FastAPI()
 keyboard = Keyboard()
 
-origins = ["http://localhost:5173"]
+origins = ["*"]
 
 app.add_middleware(
   CORSMiddleware,
@@ -17,71 +17,110 @@ app.add_middleware(
   allow_headers=["*"],
 )
 
-def format_json(data, column_name='name'):
-  new_data = {}
+def format_json(data, date_start=None, date_end=None, column_name='name', date_format='%Y-%m-%d'):
+  if column_name == 'name':
+    new_data = {}
+    for row in data:
+      new_data[row['name']] = row['count']
 
-  for row in data:
-    new_data[row[column_name]] = row['count']
+    return new_data
+
+  new_data, index_list = {}, 0
+  date_start = datetime.strptime(date_start, '%Y-%m-%d').date()
+  date_end = datetime.strptime(date_end, '%Y-%m-%d').date()
+
+  while date_start <= date_end:
+    date_list = data[index_list][column_name]
+    if date_start == date_list:
+      new_data[date_list.strftime(date_format)] = data[index_list]['count']
+      index_list += 1
+    else:
+      new_data[date_start.strftime(date_format)] = 0
+
+    date_start += timedelta(days=1)
 
   return new_data
 
 
 @app.get("/")
-def home():
-	return {"msg": "Welcome to Keylogger API, see localhost:8000/docs"}
-
-
-@app.get("/count/")
-def count(day:str = date.today()):
-  """
-    Args:
-      day format: YYYY-MM-DD
-  """
-  return keyboard.count(day)
-
-
-@app.get("/top_keys/")
-def top_keys(day:str = date.today(), amount:int = 5):
-  """
-    Args:
-      day format: YYYY-MM-DD
-  """
-  data = keyboard.get_top_keys(day, amount)
-  return format_json(data)
-
-
-@app.get("/daily_log/")
-def daily_log(day:str = date.today()):
-  """
-    Args:
-      day format: YYYY-MM-DD
-  """
-  data = keyboard.get_keys_log(day)
-  return format_json(data)
-
-
-@app.get("/interval_log/{start}/to/{end}", status_code=status.HTTP_200_OK)
-def interval_log(start:str, end:str, response:Response):
-  """
-    Args format: YYYY-MM-DD
-  """
-  data = keyboard.get_interval_log(start, end)
-  if data:
-    return format_json(data, 'date')
-
-  response.status_code = status.HTTP_404_NOT_FOUND
+def home(request: Request):
+  return { "msg": f"Welcome to Keylogger API, see {request.base_url}docs" }
 
 
 @app.get("/weekly_log/")
 def weekly_log():
   end = date.today()
   start = end - timedelta(days=6)
-  data = keyboard.get_interval_log(start, end)
+  data = keyboard.get_interval_use_count(start, end)
+  data = format_json(data, str(start), str(end), column_name='date', date_format='%a')
 
-  new_data = {}
-  for day in data:
-    str_day = str(day['date'])
-    label = datetime.strptime(str_day, '%Y-%m-%d').strftime('%a')
-    new_data[label] = day['count']
+  return data
 
-  return new_data
+
+@app.get("/get_most_used_keys/")
+def get_most_used_keys(day:str = date.today(), amount:int = 5):
+  data = keyboard.get_most_used_keys(day, amount)
+
+  return format_json(data)
+
+
+@app.get("/get_interval_most_used_keys/{start}/to/{end}/")
+def get_interval_most_used_keys(start:str, end:str, amount:int = 5):
+  data = keyboard.get_interval_most_used_keys(start, end, amount)
+
+  return format_json(data)
+
+
+@app.get("/get_all_time_most_used_keys/")
+def get_all_time_most_used_keys(amount:int = 5):
+  data = keyboard.get_all_time_most_used_keys(amount)
+
+  return format_json(data)
+
+
+@app.get("/get_used_keys/")
+def get_used_keys(day:str = date.today()):
+  data = keyboard.get_used_keys(day)
+
+  return format_json(data)
+
+
+@app.get("/get_interval_used_keys/{start}/to/{end}/")
+def get_interval_used_keys(start:str, end:str):
+  data = keyboard.get_interval_used_keys(start, end)
+  return format_json(data)
+
+
+@app.get("/get_all_time_used_keys/")
+def get_all_time_used_keys():
+  data = keyboard.get_all_time_used_keys()
+
+  return format_json(data)
+
+
+@app.get("/get_use_count/")
+def get_use_count(day:str = date.today()):
+  data = keyboard.get_use_count(day)
+
+  return data
+
+
+@app.get("/get_interval_use_count/{start}/to/{end}/")
+def get_interval_use_count(start:str, end:str):
+  data = keyboard.get_interval_use_count(start, end)
+
+  return format_json(data, start, end, column_name='date')
+
+
+@app.get("/get_all_time_use_count/")
+def get_all_time_use_count():
+  start = oldest_date()
+  end = date.today()
+  data = keyboard.get_all_time_use_count()
+
+  return format_json(data, str(start), str(end), column_name='date')
+
+
+@app.get("/oldest_date/")
+def oldest_date():
+  return keyboard.get_oldest_date()
